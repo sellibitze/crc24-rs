fac7f5a7a7032a0bb492ba3147438670dca61016
Sebastian Gesemann <s.gesemann@gmail.com>
2015-01-31 17:26:35 +0100

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #![feature(hash)]
 
 use std::default::Default;
-use std::hash::{ self, Hasher };
+use std::hash::{ self, Hasher, Writer };
 
 const INIT: u32 = 0xB704CE;
 
@@ -31,16 +31,12 @@ impl Default for Crc24Hasher {
 }
 
 impl Hasher for Crc24Hasher {
-	type Output = [u8; 3];
+	type Output = u32;
 
 	/// resets the state to the special nonzero value specified RFC2240
 	fn reset(&mut self) { self.state = INIT; }
 
-	fn finish(&self) -> [u8; 3] {
-		[(self.state >> 16) as u8,
-		 (self.state >>  8) as u8,
-		  self.state        as u8]
-	}
+	fn finish(&self) -> u32 { self.state }
 }
 
 impl hash::Writer for Crc24Hasher {
@@ -53,4 +49,50 @@ impl hash::Writer for Crc24Hasher {
 		self.state = s & 0xFFFFFF;
 	}
 }
+
+/// hashes the raw bytes using CRC-24
+/// (without including the length as part of the hash)
+pub fn hash_raw(octets: &[u8]) -> u32 {
+	let mut h: Crc24Hasher = Default::default();
+	h.write(octets);
+	h.finish()
+}
+
+#[cfg(test)]
+mod test {
+
+const CRC24_INIT: u32 =  0xb704ce;
+const CRC24_POLY: u32 = 0x1864cfb; // including x^24
+
+// directly translated from RFC2440 section 6.1.
+fn crc_octets(octets: &[u8]) -> u32 {
+	let mut crc = CRC24_INIT;
+	for &octet in octets.iter() {
+		crc ^= (octet as u32) << 16;
+		for _ in 0..8 {
+			crc <<= 1;
+			if (crc & 0x1000000) != 0 {
+				crc ^= CRC24_POLY;
+			}
+		}
+	}
+	crc & 0xFFFFFF
+}
+
+fn test_compare_impls(octets: &[u8]) -> bool {
+	let h1 = crc_octets(octets);
+	let h2 = super::hash_raw(octets);
+	h1 == h2
+}
+
+#[test]
+fn test() {
+	assert!(test_compare_impls(b""));
+	assert!(test_compare_impls(b"x"));
+	assert!(test_compare_impls(b"sg"));
+	assert!(test_compare_impls(b"crc"));
+	assert!(test_compare_impls(b"test"));
+}
+
+} // mod test
 
